aabebf007fc60dd0a9312eda70e4b017149343c5
Bestian Tang <bestian@gmail.com>
2021-02-24 20:34:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-quasar build -m pwa
+npm run build
 
 # navigate into the build output directory
-cd dist/pwa
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
